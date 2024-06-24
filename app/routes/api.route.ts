50f443c5ee441b7950b7ca3203aaa8347781f760
RafhaelHailar <rafhael4hailar@gmail.com>
2024-06-24 16:17:43 +0800

    [Feat] product route.

--- app/routes/api.route.ts
+++ app/routes/api.route.ts
@@ -1,5 +1,13 @@
 import { Router } from "express";
 
+import ProductController from "controllers/product.controller";
+
 const router: Router = Router();
+
+/**
+ *
+ *
+ */
+router.route("/products").get(ProductController.get);
 
 export default router;
