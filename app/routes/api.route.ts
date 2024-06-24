5cc069549c4a14f97ca74bafd9b4c99ee37d161f
RafhaelHailar <rafhael4hailar@gmail.com>
2024-06-24 16:42:51 +0800

    [Feat] get product route for one product.

--- app/routes/api.route.ts
+++ app/routes/api.route.ts
@@ -5,9 +5,12 @@ import ProductController from "controllers/product.controller";
 const router: Router = Router();
 
 /**
+ * @route GET /products
+ * @returns {object} 200 - Products
  *
- *
+ * @route GET /products/productId
+ * @returns {object} 200 - Product
  */
-router.route("/products").get(ProductController.get);
+router.route("/products/:productId?").get(ProductController.get);
 
 export default router;
