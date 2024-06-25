d9329755377ef513b02d776b3ffaa674d0cb2e5f
RafhaelHailar <rafhael4hailar@gmail.com>
2024-06-25 16:02:47 +0800

    [Feat] get all products docs.

--- app/routes/api.route.ts
+++ app/routes/api.route.ts
@@ -5,6 +5,7 @@ import ProductController from "@controllers/product.controller";
 const router: Router = Router();
 
 /**
+ * Get Product/s
  * @route GET /products
  * @returns {object} 200 - Products
  *
@@ -14,3 +15,38 @@ const router: Router = Router();
 router.route("/products/:productId?").get(ProductController.get);
 
 export default router;
+
+/**
+ * @swagger
+ * tags:
+ *   name: Product
+ *   description: Product API's
+ */
+
+/**
+ * @swagger
+ * /api/products:
+ *   get:
+ *     description: Return products for the menu.
+ *     summary: Get product for the menu
+ *     tags: [Product]
+ *     responses:
+ *       "200":
+ *         description: Success!
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               schema:
+ *                  $ref: '#/components/schemas/Product'
+ *               example:
+ *                  id: 346f4b84-2567-4b62-b461-edc05d71d370
+ *                  name: Som tam
+ *                  description: To prepare Thailand’s most famous salad, pound garlic and chilies with a mortar and pestle. Toss in tamarind juice, fish sauce, peanuts, dried shrimp, tomatoes, lime juice, sugar cane paste, string beans and a handful of grated green papaya. Grab a side of sticky rice. Variations include those made with crab (som tam boo) and fermented fish sauce (som tam plah lah), but none matches the flavor and simple beauty of the original.
+ *                  cookingTimeInSec: 2000
+ *                  categoryId: 1222ee11-9148-4ac2-8a67-3066336b28b0
+ *                  category:
+ *                      id: 1222ee11-9148-4ac2-8a67-3066336b28b0
+ *                      name: Exquisite
+ *                      status: false
+ */
