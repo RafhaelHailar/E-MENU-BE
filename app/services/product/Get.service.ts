6ad15f73872df4057ac4f301ac7637da3e9a84d5
RafhaelHailar <rafhael4hailar@gmail.com>
2024-06-25 10:23:35 +0800

    [Fix] path not properly set.

--- app/services/product/Get.service.ts
+++ app/services/product/Get.service.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import prisma from "@../prisma";
+import prisma from "@/../prisma";
 
 async function GetService(req: Request, res: Response) {
   const { productId } = req.params;
