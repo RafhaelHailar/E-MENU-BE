ee3627f35fe80d36a501cbe6a542ec03d6828aed
RafhaelHailar <rafhael4hailar@gmail.com>
2024-06-25 16:12:52 +0800

    [Update] added get product docs.

--- app/routes/api.route.ts
+++ app/routes/api.route.ts
@@ -28,8 +28,44 @@ export default router;
  * /api/products:
  *   get:
  *     description: Return products for the menu.
- *     summary: Get product for the menu
+ *     summary: Get all products for the menu
  *     tags: [Product]
+ *     responses:
+ *       "200":
+ *         description: Success!
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               schema:
+ *                  $ref: '#/components/schemas/Product'
+ *               example:
+ *                  id: 346f4b84-2567-4b62-b461-edc05d71d370
+ *                  name: Som tam
+ *                  description: To prepare Thailand’s most famous salad, pound garlic and chilies with a mortar and pestle. Toss in tamarind juice, fish sauce, peanuts, dried shrimp, tomatoes, lime juice, sugar cane paste, string beans and a handful of grated green papaya. Grab a side of sticky rice. Variations include those made with crab (som tam boo) and fermented fish sauce (som tam plah lah), but none matches the flavor and simple beauty of the original.
+ *                  cookingTimeInSec: 2000
+ *                  categoryId: 1222ee11-9148-4ac2-8a67-3066336b28b0
+ *                  category:
+ *                      id: 1222ee11-9148-4ac2-8a67-3066336b28b0
+ *                      name: Exquisite
+ *                      status: false
+ */
+
+/**
+ * @swagger
+ * /api/products/{productId}:
+ *   get:
+ *     description: Returns a single product.
+ *     summary: Get a product for the menu.
+ *     tags: [Product]
+ *     parameters:
+ *      - name: productId
+ *        in: path
+ *        description: ID of product to return
+ *        required: true
+ *        schema:
+ *          type: string
+ *          format: uuid
  *     responses:
  *       "200":
  *         description: Success!
