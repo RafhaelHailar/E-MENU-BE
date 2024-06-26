c7f9f1bee017d1890e098d06f8eeae7b77a17634
RafhaelHailar <rafhael4hailar@gmail.com>
2024-06-26 11:26:21 +0800

    [Update] update cart service

---
+++ app/services/order/UpdateCart.service.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from "express";
+import prisma from "@/../prisma";
+
+async function UpdateCartService(req: Request, res: Response) {}
+
+export default UpdateCartService;
