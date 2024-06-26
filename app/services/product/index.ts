1bec3d84065d38e8a1ad59cb7a199405dd6bdd08
RafhaelHailar <rafhael4hailar@gmail.com>
2024-06-26 12:26:16 +0800

    [Update] add categorize product in shortcut.

--- app/services/product/index.ts
+++ app/services/product/index.ts
@@ -1,3 +1,4 @@
 export { default as GetProductService } from "./Get.service";
 export { default as AddProductService } from "./Add.service";
 export { default as AddProductCategoryService } from "./AddProductCategory.service";
+export { default as AddProductCategorizeService } from "./CategorizeProduct.service";
