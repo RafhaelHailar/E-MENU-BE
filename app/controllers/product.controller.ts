2176b4dca6d8cd3cff7126456bc95d42d6fd26b6
RafhaelHailar <rafhael4hailar@gmail.com>
2024-06-26 12:07:10 +0800

    [Update] add product category controller

--- app/controllers/product.controller.ts
+++ app/controllers/product.controller.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express";
 import asyncHandler from "@utils/asyncHandler";
-import { GetProductService, AddProductService } from "@services/product";
+import {
+  GetProductService,
+  AddProductService,
+  AddProductCategoryService,
+} from "@services/product";
 
 const ProductController = {
   get: asyncHandler(async (req: Request, res: Response) => {
@@ -9,6 +13,10 @@ const ProductController = {
 
   add: asyncHandler(async (req: Request, res: Response) => {
     await AddProductService(req, res);
+  }),
+
+  addProductCategory: asyncHandler(async (req: Request, res: Response) => {
+    await AddProductCategoryService(req, res);
   }),
 };
 
