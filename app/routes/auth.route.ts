626f6319371ccb91486c7c9263ac7cb99cb70c6f
RafhaelHailar <rafhael4hailar@gmail.com>
2024-06-27 10:58:13 +0800

    [Feat] register user.

--- app/routes/auth.route.ts
+++ app/routes/auth.route.ts
@@ -1,5 +1,8 @@
 import { Router } from "express";
+import AuthController from "@controllers/auth.controller";
 
 const router: Router = Router();
+
+router.get("/register/:tableId", AuthController.register);
 
 export default router;
