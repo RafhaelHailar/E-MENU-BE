f9afed4aecf7079683c1306972d18f8a3f191d31
RafhaelHailar <rafhael4hailar@gmail.com>
2024-06-27 09:26:14 +0800

    [Update] made categories an array of strings to easy access.

--- app/services/product/Get.service.ts
+++ app/services/product/Get.service.ts
@@ -1,5 +1,20 @@
 import { Request, Response } from "express";
 import prisma from "@/../prisma";
+import type { ProductWithCategory } from "@./types/prismaExtended";
+
+function transformProduct(product: ProductWithCategory) {
+  const categories = product.productCategorize.map((categorize) => {
+    return categorize.category.name;
+  });
+
+  const transformedProduct = {
+    ...product,
+    categories,
+    ratings: 5,
+  };
+
+  return transformedProduct;
+}
 
 async function GetService(req: Request, res: Response) {
   const { productId } = req.params;
@@ -33,7 +48,15 @@ async function GetService(req: Request, res: Response) {
     },
   });
 
-  return res.status(200).json(products);
+  const transformedProducts = [];
+
+  for (let i = 0; i < products.length; i++) {
+    const product = products[i];
+    const transformedProduct = transformProduct(product as ProductWithCategory);
+    transformedProducts.push(transformedProduct);
+  }
+
+  return res.status(200).json(transformedProducts);
 }
 
 export default GetService;
