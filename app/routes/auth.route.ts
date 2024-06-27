21480eed4722e3554084cd4de7e07b315e53992c
RafhaelHailar <rafhael4hailar@gmail.com>
2024-06-27 13:17:45 +0800

    [Feat] update table registration

--- app/routes/auth.route.ts
+++ app/routes/auth.route.ts
@@ -3,6 +3,14 @@ import AuthController from "@controllers/auth.controller";
 
 const router: Router = Router();
 
-router.get("/register/:tableId", AuthController.register);
+/**
+ * Register a user to a table
+ * @route POST /order/{tableId}
+ *
+ * @returns {object} 404 - Table with given table id is not found
+ * @returns {object} 403 - There is another session open
+ * @returns {object} 200 - Table session key
+ */
+router.get("/order/:tableId", AuthController.register);
 
 export default router;
