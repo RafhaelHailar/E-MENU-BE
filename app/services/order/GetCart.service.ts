11511706ae274a2cdef678e7d7295fac98c2dc8c
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-06-29 14:35:40 +0800

    [Feat] get cart items.

---
+++ app/services/order/GetCart.service.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from "express";
+import prisma from "@/../prisma";
+
+async function GetCartService(req: Request, res: Response) {
+  const { customerId, tableId } = req.tableSession as Request["tableSession"];
+
+  const customer = await prisma.customer.findUnique({
+    where: {
+      id: customerId,
+    },
+    include: {
+      cart: true,
+    },
+  });
+
+  if (customer)
+    return res
+      .status(401)
+      .json({ message: "customer with the given id not found" });
+  if (customer.tableId !== tableId)
+    return res.status(409).json({ message: "table id desrepancy." });
+
+  const cart = await prisma.cart.findUnique({
+    where: {
+      customerId,
+    },
+  });
+
+  const cartItems = await prisma.cartItem.findMany({
+    where: {
+      cartId: cart.id,
+    },
+  });
+
+  return res.status(200).json(cartItems);
+}
+
+export default GetCartService;
