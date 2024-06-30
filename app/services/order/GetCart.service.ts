abd64cd1f781e0958e1179df92e9f04c36b89e8f
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-06-30 19:08:05 +0800

    [Update] removed condition

--- app/services/order/GetCart.service.ts
+++ app/services/order/GetCart.service.ts
@@ -3,10 +3,6 @@ import prisma from "@/../prisma";
 
 async function GetCartService(req: Request, res: Response) {
   const tableSession = req.cookies._table_session;
-  const tableNo = req.cookies._table_no;
-
-  if (!tableSession || !tableNo)
-    return res.status(400).json({ message: "please authenticate" });
 
   const cartItems = await prisma.cartItem.findMany({
     where: {
