d4ea6b25bb9fc18eca0af7f9ef22f59f31e30d62
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-06-30 15:05:59 +0800

    [Update] check for authentication

--- app/middlewares/auth.ts
+++ app/middlewares/auth.ts
@@ -1,55 +1,22 @@
 import { Request, Response, NextFunction } from "express";
 import prisma from "@/../prisma";
 
-const auth = ({ isResponding = true } = {}) => {
-  return async function (req: Request, res: Response, next: NextFunction) {
-    const tableSession = req.cookies._table_session;
+const auth = async (req: Request, res: Response, next: NextFunction) => {
+  const tableSession = req.cookies._table_session;
 
-    if (!tableSession) {
-      const error = { status: 401, message: "please authenticate" };
-      if (isResponding)
-        return res.status(error.status).json({ message: error.message });
-      else {
-        req.tableSession = {
-          error: {
-            status: error.status,
-            message: error.message,
-          },
-        };
-        return next();
-      }
-    }
+  const session = await prisma.table.findUnique({
+    where: {
+      session: tableSession,
+    },
+  });
 
-    const [customerId, tableId] = tableSession.split(".");
+  if (!session) return res.status(404).json({ message: "session not found" });
 
-    const customer = await prisma.customer.findUnique({
-      where: {
-        id: customerId,
-      },
-    });
+  if (!session.status)
+    return res.status(401).json({ message: "session not approved" });
 
-    if (!customer) {
-      const error = {
-        status: 401,
-        message: "table with that session is not found!",
-      };
-      if (isResponding)
-        return res.status(error.status).json({ message: error.message });
-      else {
-        req.tableSession = {
-          error: {
-            status: error.status,
-            message: error.message,
-          },
-        };
-        return next();
-      }
-    }
-
-    const sessionData = { customerId, tableId: Number(tableId) };
-    req.tableSession = sessionData;
-    next();
-  };
+  req.tableSession = session;
+  next();
 };
 
 export default auth;
