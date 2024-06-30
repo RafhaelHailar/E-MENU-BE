2e23c49c7e50395282300b8d535d6733ebfab3fe
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-06-30 15:04:49 +0800

    [Update] register in new way.

--- app/services/auth/Register.service.ts
+++ app/services/auth/Register.service.ts
@@ -1,55 +1,23 @@
 import { Request, Response } from "express";
 import prisma from "@/../prisma";
+import crypto from "crypto";
 
 async function RegisterService(req: Request, res: Response) {
   const tableId = Number(req.params.tableId);
 
-  const table = await prisma.table.findUnique({
-    where: {
-      id: tableId,
-    },
-    include: {
-      customers: true,
-    },
-  });
+  const sessionId = crypto.randomBytes(32).toString("hex");
 
-  if (!table)
-    return res.status(404).json({ message: "no table with that id found!" });
+  res.cookie("_table_session", sessionId);
+  res.cookie("_table_no", tableId);
 
-  if (req.tableSession && !req.tableSession.error) {
-    const tableSession = req.tableSession as Request["tableSession"];
-
-    const customer = await prisma.customer.findUnique({
-      where: {
-        id: tableSession.customerId,
-      },
-    });
-
-    if (customer) {
-      if (customer.tableId !== tableId)
-        return res
-          .status(409)
-          .json({ message: "you are in session in other table" });
-      return res.redirect("http://localhost:3000");
-    }
-  }
-
-  if (table.customers.length !== 0)
-    return res.status(403).json({ message: "there is another session open" });
-
-  const customer = await prisma.customer.create({
+  await prisma.table.create({
     data: {
-      name: `T${tableId} Leader`,
-      role: "LEADER",
-      cart: { create: {} },
-      tableId,
+      session: sessionId,
+      tableNo: tableId,
     },
   });
 
-  res.cookie("_table_session", customer.id + "." + tableId);
-
-  // res.status(200).json({ table_session: customer.id + "." + tableId });
-  return res.redirect("http://localhost:3000");
+  return res.redirect(process.env.FRONTEND_BASE_URL);
 }
 
 export default RegisterService;
