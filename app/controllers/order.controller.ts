51b0abc2f31c7003304d466c9f3b37218cdcde28
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-06-30 10:03:30 +0800

    [Feat] checkout controller

--- app/controllers/order.controller.ts
+++ app/controllers/order.controller.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express";
 import asyncHandler from "@utils/asyncHandler";
-import { GetCartService, UpdateCartService } from "@services/order";
+import {
+  CheckoutService,
+  GetCartService,
+  UpdateCartService,
+} from "@services/order";
 
 const OrderController = {
   updateCart: asyncHandler(async (req: Request, res: Response) => {
@@ -9,6 +13,10 @@ const OrderController = {
 
   getCart: asyncHandler(async (req: Request, res: Response) => {
     await GetCartService(req, res);
+  }),
+
+  checkout: asyncHandler(async (req: Request, res: Response) => {
+    await CheckoutService(req, res);
   }),
 };
 
