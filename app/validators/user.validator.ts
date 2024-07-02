9135670859a6b8e6867a85a18ee39c6bafbf8316
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-02 10:04:16 +0800

    [Feat] validate request body in user creation

---
+++ app/validators/user.validator.ts
@@ -0,0 +1,19 @@
+import { roles } from "@config/roles";
+import Joi from "joi";
+import { password } from "./password.validator";
+
+const create = {
+  body: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    contact: Joi.string().required(),
+    password: Joi.string().custom(password),
+    role: Joi.string().valid(roles),
+  }),
+};
+
+const userValidators = {
+  create,
+};
+
+export default userValidators;
