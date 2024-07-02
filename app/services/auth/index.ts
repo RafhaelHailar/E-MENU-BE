d6f19a0d66711fcc90f9f9b57751df10a5893b20
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-02 10:35:24 +0800

    [Update] added login

--- app/services/auth/index.ts
+++ app/services/auth/index.ts
@@ -1,4 +1,4 @@
 export { default as RegisterTableService } from "./RegisterTable.service";
 export { default as GroupRegisterService } from "./GroupRegister.service";
-export { default as ListQueuesService } from "./ListQueues.service";
 export { default as RegisterService } from "./Register.service";
+export { default as LoginService } from "./Login.service";
