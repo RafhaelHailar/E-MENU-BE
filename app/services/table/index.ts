ab214961a945f71641a4da1c9455c35ed5fdc586
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-02 11:11:32 +0800

    [Update] move table registration shortucts

--- app/services/table/index.ts
+++ app/services/table/index.ts
@@ -1,1 +1,3 @@
 export { default as ListQueuesService } from "./ListQueues.service";
+export { default as RegisterService } from "./Register.service";
+export { default as GroupRegisterService } from "../table/GroupRegister.service";
