76afcad6ee43c02e59a2627602837bed5ecd3fbd
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-02 10:06:44 +0800

    [Feat] register user

--- app/routes/auth.route.ts
+++ app/routes/auth.route.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import AuthController from "@controllers/auth.controller";
 import auth from "@middlewares/auth";
+import userValidators from "@validators/user.validator";
+import validate from "@middlewares/validate";
 
 const router: Router = Router();
 
@@ -12,7 +14,7 @@ const router: Router = Router();
  * @returns {object} 403 - There is another session open
  * @returns {object} 200 - Table session key
  */
-router.get("/order/:tableId", AuthController.register);
+router.get("/order/:tableId", AuthController.tableRegister);
 
 /**
  * Register a user to a group
@@ -31,5 +33,18 @@ router.get("/group/:_session_id", AuthController.groupRegister);
  * @returns {object} 200 - List of Table Session Requests
  */
 router.get("/session/queues", auth("getSessions"), AuthController.listQueues);
+
+/**
+ * Register User
+ * @route POST /register
+ *
+ * @returns {object} 409 - Email given is already taken.
+ * @returns {object} 200 - User is Registered
+ */
+router.post(
+  "/register",
+  validate(userValidators.create),
+  AuthController.register,
+);
 
 export default router;
