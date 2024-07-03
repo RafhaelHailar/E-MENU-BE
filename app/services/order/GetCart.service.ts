2e3de1b008eebbbc8143395fce303ae211220643
RafhaelHailar <rafhael4hailar@gmail.com>
2024-07-03 08:17:51 +0800

    [Update] check if there is table session

--- app/services/order/GetCart.service.ts
+++ app/services/order/GetCart.service.ts
@@ -4,6 +4,8 @@ import prisma from "@/../prisma";
 async function GetCartService(req: Request, res: Response) {
   const tableSession = req.cookies._table_session;
 
+  if (!tableSession) return res.status(200).json([]);
+
   const cartItems = await prisma.cartItem.findMany({
     where: {
       sessionId: tableSession,
