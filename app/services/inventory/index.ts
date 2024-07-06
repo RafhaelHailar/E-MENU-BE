16556447572e650f08c25c7134fdac54068602e6
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-06 08:34:17 +0800

    [Update] added delete inventory item

--- app/services/inventory/index.ts
+++ app/services/inventory/index.ts
@@ -1,3 +1,4 @@
 export { default as GetItemsService } from "./GetItems.service";
 export { default as AddItemService } from "./AddItem.service";
 export { default as UpdateItemService } from "./UpdateItem.service";
+export { default as DeleteItemService } from "./DeleteItem.service";
