7868e3bcc0a5d07b2e92d6a8370a09ba71ea15e6
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-06 18:34:38 +0800

    [Feat] update product service

---
+++ app/services/product/Update.service.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from "express";
+import prisma from "@/../prisma";
+
+async function UpdateService(req: Request, res: Response) {
+  const { id, name, image, price, estimatedCookingTimeMin } = req.body;
+
+  const product = await prisma.product.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  if (!product)
+    return res
+      .status(404)
+      .json({ message: "product with given id is not found" });
+
+  await prisma.product.update({
+    where: {
+      id,
+    },
+    data: {
+      name,
+      image,
+      price,
+      estimatedCookingTimeMin,
+    },
+  });
+
+  return res.status(200).json({ message: "product is sucessfully updated" });
+}
+
+export default UpdateService;
