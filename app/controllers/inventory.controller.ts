2da2c63a3ed23dd1345bef8b691a5be359a86f4f
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-06 08:10:20 +0800

    [Feat] add inventory item controller

--- app/controllers/inventory.controller.ts
+++ app/controllers/inventory.controller.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from "express";
 import asyncHandler from "@utils/asyncHandler";
-import { GetItemsService } from "@services/inventory";
+import { GetItemsService, AddItemService } from "@services/inventory";
 
 const InventoryController = {
   getItems: asyncHandler(async (req: Request, res: Response) => {
     await GetItemsService(req, res);
+  }),
+
+  addItem: asyncHandler(async (req: Request, res: Response) => {
+    await AddItemService(req, res);
   }),
 };
 
