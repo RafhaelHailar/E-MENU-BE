c044fbcade51f5b29da59f70194525ba923debc8
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-07 17:25:35 +0800

    [Feat] get customer loyalty

--- app/controllers/loyalty.controller.ts
+++ app/controllers/loyalty.controller.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from "express";
 import asyncHandler from "@utils/asyncHandler";
-import { GetService } from "@services/loyalty";
+import { GetService, GetMyLoyaltiesService } from "@services/loyalty";
 
 const LoyaltyController = {
   get: asyncHandler(async (req: Request, res: Response) => {
     await GetService(req, res);
+  }),
+
+  getMyLoyalties: asyncHandler(async (req: Request, res: Response) => {
+    await GetMyLoyaltiesService(req, res);
   }),
 };
 
