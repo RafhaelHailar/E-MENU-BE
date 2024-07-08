d3fc6f7e39deade8489a7904af14fc50494f97a6
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-08 20:26:39 +0800

    [Update] added other roles

--- app/config/roles.ts
+++ app/config/roles.ts
@@ -2,6 +2,10 @@ import { User_Role } from "@prisma/client";
 
 const allRoles = {
   [User_Role.ADMIN]: ["manageProducts", "getSessions"],
+  [User_Role.CASHIER]: [],
+  [User_Role.KITCHEN]: [],
+  [User_Role.WAITER]: [],
+  [User_Role.CUSTOMER]: [],
 };
 
 export const roles = Object.keys(allRoles);
