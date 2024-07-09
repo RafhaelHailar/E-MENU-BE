439c73284b10e9e31fd7497030c52b25b7073eac
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-09 13:44:50 +0800

    [Update] added get customer debits

--- app/services/loyalty/index.ts
+++ app/services/loyalty/index.ts
@@ -1,2 +1,3 @@
 export { default as GetService } from "./Get.service";
 export { default as GetMyLoyaltiesService } from "./GetMyLoyalties.service";
+export { default as GetMyDebitsService } from "./GetMyDebits.service";
