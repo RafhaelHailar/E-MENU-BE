65509530b394abda7cb7e943cd5e733d9fa65144
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-09 17:52:46 +0800

    [Update] added customer email login

--- app/services/auth/index.ts
+++ app/services/auth/index.ts
@@ -1,2 +1,3 @@
 export { default as RegisterService } from "./Register.service";
 export { default as LoginService } from "./Login.service";
+export { default as CustomerLoginService } from "./CustomerLogin.service";
