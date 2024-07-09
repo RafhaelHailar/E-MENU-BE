88b1aa7b9ef8f1f1f0ced78ecb5b76b2bb2e2be5
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-09 13:51:05 +0800

    [Feat] get total loyalty points

--- app/controllers/loyalty.controller.ts
+++ app/controllers/loyalty.controller.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from "express";
 import asyncHandler from "@utils/asyncHandler";
-import { GetService, GetMyLoyaltiesService } from "@services/loyalty";
+import {
+  GetService,
+  GetMyLoyaltiesService,
+  GetMyDebitsService,
+} from "@services/loyalty";
+import { Debit, Loyalty } from "@prisma/client";
 
 const LoyaltyController = {
   get: asyncHandler(async (req: Request, res: Response) => {
@@ -8,7 +13,40 @@ const LoyaltyController = {
   }),
 
   getMyLoyalties: asyncHandler(async (req: Request, res: Response) => {
-    await GetMyLoyaltiesService(req, res);
+    try {
+      const loyaltyPoints = await GetMyLoyaltiesService(req);
+      return res.status(200).json(loyaltyPoints);
+    } catch (e) {
+      return res.status(e.statusCode).json({ message: e.message });
+    }
+  }),
+
+  getMyDebits: asyncHandler(async (req: Request, res: Response) => {
+    try {
+      const debits = await GetMyDebitsService(req);
+      return res.status(200).json(debits);
+    } catch (e) {
+      return res.status(e.statusCode).json({ message: e.message });
+    }
+  }),
+
+  getMyTotalLoyalties: asyncHandler(async (req: Request, res: Response) => {
+    try {
+      const loyaltyPoints = (await GetMyLoyaltiesService(req)) as Loyalty[];
+      const debits = (await GetMyDebitsService(req)) as Debit[];
+
+      const totalPoints = loyaltyPoints.reduce(
+        (total, loyalty) => total + loyalty.amount,
+        0,
+      );
+      const totalDebits = debits.reduce(
+        (total, debit) => total + debit.amount,
+        0,
+      );
+      return res.status(200).json(totalPoints - totalDebits);
+    } catch (e) {
+      return res.status(e.statusCode).json({ message: e.message });
+    }
   }),
 };
 
