22bd021b27acfa536cf3d2562b33402de14f50ae
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-09 18:18:10 +0800

    [Feat] verify email code

---
+++ app/services/auth/VerifyCode.service.ts
@@ -0,0 +1,46 @@
+import { Request, Response } from "express";
+import prisma from "@/../prisma";
+import crypto from "crypto";
+import bcrypt from "bcrypt";
+
+async function VerifyCodeService(req: Request, res: Response) {
+  const { email } = req.cookies._customer_email;
+  const { code } = req.body.code;
+
+  const otp = await prisma.oTP.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (!otp)
+    return res
+      .status(404)
+      .json({ message: "given email does not request for verification" });
+
+  const isMatch = otp.code === code;
+
+  if (!isMatch)
+    return res.status(401).json({ message: "given code is not valid" });
+
+  const salt = await bcrypt.genSalt();
+  const sessionId = await bcrypt.hash(
+    crypto.randomBytes(32).toString("hex"),
+    salt,
+  );
+
+  await prisma.oTP.update({
+    where: {
+      email,
+      code,
+    },
+    data: {
+      sessionId,
+    },
+  });
+
+  res.cookie("_loyalty_session", sessionId);
+  return res.status(200).json({ message: "email is verified" });
+}
+
+export default VerifyCodeService;
