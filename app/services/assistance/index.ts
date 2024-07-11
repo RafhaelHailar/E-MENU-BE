8baae81c2cd4880910472072915240ad4f0b2b2d
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-11 23:37:34 +0800

    [Update] added decline request

--- app/services/assistance/index.ts
+++ app/services/assistance/index.ts
@@ -1,3 +1,4 @@
 export { default as RequestService } from "./Request.service";
 export { default as ApproveRequestService } from "./ApproveRequest.service";
+export { default as DeclineRequestService } from "./DeclineRequest.service";
 export { default as ListRequestsService } from "./ListRequests.service";
