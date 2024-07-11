32dfb6befcd6930a5706d6777943ab9f66809c2d
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-11 23:33:24 +0800

    [Update] added list requests

--- app/services/assistance/index.ts
+++ app/services/assistance/index.ts
@@ -1,2 +1,3 @@
 export { default as RequestService } from "./Request.service";
 export { default as ApproveRequestService } from "./ApproveRequest.service";
+export { default as ListRequestsService } from "./ListRequests.service";
