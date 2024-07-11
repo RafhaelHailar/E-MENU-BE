e449f1972581b1514c7daa3824f612899bbaefbf
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-11 23:26:43 +0800

    [Feat] approve request controller

--- app/controllers/assitance.controller.ts
+++ app/controllers/assitance.controller.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from "express";
 import asyncHandler from "@utils/asyncHandler";
-import { RequestService } from "@services/assistance";
+import { ApproveRequestService, RequestService } from "@services/assistance";
 
 const AssistanceController = {
   request: asyncHandler(async (req: Request, res: Response) => {
     await RequestService(req, res);
+  }),
+
+  approveRequest: asyncHandler(async (req: Request, res: Response) => {
+    await ApproveRequestService(req, res);
   }),
 };
 
