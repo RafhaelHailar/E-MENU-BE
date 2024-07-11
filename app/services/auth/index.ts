b7aba2287903b203cb4a4ec7899b41b7b7dd13e6
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-11 13:57:22 +0800

    [Update] added logout

--- app/services/auth/index.ts
+++ app/services/auth/index.ts
@@ -2,3 +2,4 @@ export { default as RegisterService } from "./Register.service";
 export { default as LoginService } from "./Login.service";
 export { default as CustomerLoginService } from "./CustomerLogin.service";
 export { default as VerifyCodeService } from "./VerifyCode.service";
+export { default as LogoutService } from "./Logout.service";
