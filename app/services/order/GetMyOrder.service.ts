83e05df3d94eb600ceb0c0d629dc6ff45a9084a8
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-16 06:13:22 +0800

    [Perf] improve getting orders performance

--- app/services/order/GetMyOrder.service.ts
+++ app/services/order/GetMyOrder.service.ts
@@ -5,55 +5,36 @@ async function GetMyOrdersService(req: Request, res: Response) {
   const tableSession = req.tableSession.session;
   const tableNo = Number(req.tableSession.tableNo);
 
-  const orders = await prisma.orders.findMany({
+  const orders = await prisma.transactions.findMany({
     where: {
       sessionId: tableSession,
       tableNo,
     },
-    include: {
-      product: true,
+    select: {
+      status: true,
+      orderNo: true,
+      transactionId: true,
+      createdAt: true,
+      orders: {
+        select: {
+          id: true,
+          sessionId: true,
+          price: true,
+          quantity: true,
+          amount: true,
+          createdAt: true,
+          product: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
+        },
+      },
     },
   });
 
-  const ordersByTransactionId = [];
-  const cacheTransaction = {};
-
-  for (let i = 0; i < orders.length; i++) {
-    const order = orders[i];
-    let transaction;
-
-    if (cacheTransaction[order.transactionId]) {
-      transaction = cacheTransaction[order.transactionId];
-    } else {
-      transaction = await prisma.transactions.findUnique({
-        where: {
-          transactionId: order.transactionId,
-        },
-      });
-      cacheTransaction[order.transactionId] = transaction;
-    }
-
-    const group = ordersByTransactionId.find(
-      (orderGroup) => orderGroup.transactionId === order.transactionId,
-    );
-
-    if (group) {
-      if (order.createdAt < group.orderDate) group.orderDate = order.createdAt;
-      group.orders.push(order);
-      group.total += order.amount;
-    } else {
-      ordersByTransactionId.push({
-        transactionId: order.transactionId,
-        orderNo: order.orderNo,
-        orders: [order],
-        orderDate: order.createdAt,
-        total: order.amount,
-        status: transaction.status,
-      });
-    }
-  }
-
-  return res.status(200).json(ordersByTransactionId);
+  return res.status(200).json(orders);
 }
 
 export default GetMyOrdersService;
