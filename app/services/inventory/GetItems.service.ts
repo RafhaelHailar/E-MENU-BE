7600464d049afa0a271d99fb3420ebed5d87ba2b
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-15 18:33:57 +0800

    [Update] temporarily removed inventory api

--- app/services/inventory/GetItems.service.ts
+++ app/services/inventory/GetItems.service.ts
@@ -2,8 +2,8 @@ import { Request, Response } from "express";
 import prisma from "@/../prisma";
 
 const GetItemsService = async (req: Request, res: Response) => {
-  const items = await prisma.inventoryItem.findMany();
-  return res.status(200).json(items);
+  /*   const items = await prisma.inventoryItem.findMany();
+  return res.status(200).json(items); */
 };
 
 export default GetItemsService;
