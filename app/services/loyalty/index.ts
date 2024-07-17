5d264e1122320e3aa5bd533e66f52c4977de7994
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-18 00:34:23 +0800

    [Update] added get rewards

--- app/services/loyalty/index.ts
+++ app/services/loyalty/index.ts
@@ -4,3 +4,4 @@ export { default as GetMyDebitsService } from "./GetMyDebits.service";
 export { default as GetMyTotalLoyaltiesService } from "./GetMyTotalLoyalties.service";
 export { default as RedeemService } from "./Redeem.service";
 export { default as CreateRewardService } from "./CreateReward.service";
+export { default as GetRewardsService } from "./GetRewards.service";
