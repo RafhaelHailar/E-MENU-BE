17f0b5a32dce821ffcd1ee076845dbc30550d979
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-18 00:49:16 +0800

    [Update] added delete reward

--- app/services/loyalty/index.ts
+++ app/services/loyalty/index.ts
@@ -6,3 +6,4 @@ export { default as RedeemService } from "./Redeem.service";
 export { default as CreateRewardService } from "./CreateReward.service";
 export { default as GetRewardsService } from "./GetRewards.service";
 export { default as UpdateRewardService } from "./UpdateReward.service";
+export { default as DeleteRewardService } from "./DeleteReward.service";
