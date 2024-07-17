e71e3ac7506b519fc5af12aadf40b19c4856e8da
Rafhael Hailar <rafhael4hailar@gmail.com>
2024-07-17 13:54:38 +0800

    [Update] get product inventory

--- app/services/inventory/GetItems.service.ts
+++ app/services/inventory/GetItems.service.ts
@@ -2,8 +2,23 @@ import { Request, Response } from "express";
 import prisma from "@/../prisma";
 
 const GetItemsService = async (req: Request, res: Response) => {
-  /*   const items = await prisma.inventoryItem.findMany();
-  return res.status(200).json(items); */
+  const items = await prisma.product.findMany({
+    select: {
+      id: true,
+      inventory: {
+        select: {
+          quantity: true,
+        },
+      },
+    },
+  });
+
+  const mappedItems = items.map(({ id, inventory }) => ({
+    id,
+    quantity: inventory.quantity,
+  }));
+
+  return res.status(200).json(mappedItems);
 };
 
 export default GetItemsService;
